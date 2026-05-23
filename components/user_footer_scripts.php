bd0e5b59c3ef785ce105fe42a2b8c8e9552dfd89
Devanantha <madedevanantha@gmail.com>
2026-05-23 22:20:37 +0800

    Adding footer scripts

---
+++ components/user_footer_scripts.php
@@ -0,0 +1,10 @@
+<!-- FOOTER SCRIPTS USER -->
+<script src="assets/js/api.js?v=4"></script>
+<?php if (!empty($extraJs)): ?>
+<?php foreach ((array)$extraJs as $js): ?>
+<script src="<?= $js ?>?v=4"></script>
+<?php endforeach; ?>
+<?php endif; ?>
+<script src="assets/js/user-app.js?v=4"></script>
+</body>
+</html>
