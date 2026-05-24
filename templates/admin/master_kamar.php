9b2b455540cbd992cbea49947eaa5d6fac24d822
absukatidur <andika.azk96@gmail.com>
2026-05-24 20:54:11 +0800

    Adding master_kamar.php into admin interface

---
+++ templates/admin/master_kamar.php
@@ -0,0 +1,20 @@
+<template id="tpl-dashboard-page">
+  </template>
+
+<template id="tpl-stat-card">
+  </template>
+
+<template id="tpl-chart-bar">
+  </template>
+
+<template id="tpl-recent-order-row">
+  </template>
+
+<template id="tpl-activity-item">
+  </template>
+
+<template id="tpl-room-cell">
+  </template>
+
+<template id="tpl-legend-item">
+  </template>
